ab1085d0ba437934730348c1962be9d168d3e7ab
takahiro <paperback63@gmail.com>
2025-04-18 13:12:45 +0900

    zipcodeのjsを独立させ、全ページでaddEventListenerを探そうとするエラーを解除

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('script')
+    @vite(['resources/js/zipcode.js'])
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
